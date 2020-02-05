72ee2c625578ec82d9b6ce003b8c932db021bcc9
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2020-02-06 00:55:20 +0530

    Added new endpoint urls

--- src/main/java/io/github/adorableskullmaster/pw4j/enums/QueryURL.java
+++ src/main/java/io/github/adorableskullmaster/pw4j/enums/QueryURL.java
@@ -6,6 +6,7 @@ public enum QueryURL {
   NATION_URL("nation/id={0}&key={1}"),
   NATIONS_URL("nations/?key={0}"),
   ALLIANCE_URL("alliance/id={0}&key={1}"),
+    ALLIANCE_MEMBERS_URL("alliance-members/?allianceid={0}&key={1}"),
   ALLIANCES_URL("alliances/?key={0}"),
   WAR_URL("war/{0}&key={1}"),
   WARS_URL("wars/?key={0}"),
@@ -16,7 +17,8 @@ public enum QueryURL {
   TRADEPRICE_URL("tradeprice/resource={0}&key={1}"),
   TRADEHISTORY_URL("trade-history/?key={0}"),
   ALL_CITIES_URL("all-cities/?key={0}"),
-  NATION_MILITARY_URL("nation-military/?key={0}");
+    NATION_MILITARY_URL("nation-military/?key={0}"),
+    WAR_ATTACKS_URL("war-attacks/key={0}");
 
   private final String url;
 
